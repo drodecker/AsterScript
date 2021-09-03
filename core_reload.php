b071578e16a37cfc27e8a80ba3d4e240e00c9fdf
drodecker <dave@rodecker.com>
2021-09-02 17:42:16 -0700

    Reload upon button click
    To prevent inadvertently having the script run, the web page has a button to click.

--- core_reload.php
+++ core_reload.php
@@ -1,20 +1,30 @@
+<html>
+<title>AsterScript</title>
+<body>
+Issue an AMI Asterisk script.<br>
+<form action="core_reload.php" method="post">
+    <input type="submit" name="reload" value="reload" onclick="reload()" />
+</form>
+
 <?php
 # core_reload.php
 # This forces refresh of asterisk to reload all extensions and scripts
-# Note that /etc/asterisk/manager.conf needs to be edited to contain a set of confirmations for [resetuser]
+# Note that /etc/asterisk/manager.conf needs to be edited to contain a set of confirmations for [reloaduser]
 #[resetuser]
-#secret=resuteser
+#secret=resudaoler
 #read=system,call,log,verbose,command,agent,user
 #write=system,call,log,verbose,command,agent,user
 #deny=0.0.0.0/0.0.0.0
 #permit=127.0.0.1/255.255.255.255
 
+if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['reload'])) reload();
+function reload() {
   $oSocket = fsockopen("127.0.0.1", 5038, $errnum, $errdesc) or die("Connection Failed");
   $line = fgets($oSocket);
   echo "<br>Open socket returned: <b> $line </b><br>";
   $cmd ="Action:Login\r\n";
-  $cmd .="UserName:resetuser\r\n";
-  $cmd .="Secret:resuteser\r\n";
+  $cmd .="UserName:reloaduser\r\n";
+  $cmd .="Secret:resudaoler\r\n";
   $cmd .="\r\n";
   $cmd .="Action:Command\r\n";
   $cmd .="Command:reload\r\n";
@@ -34,4 +44,5 @@
   fclose($oSocket);
 
   echo "<br>Completed script";
+}
 ?>
